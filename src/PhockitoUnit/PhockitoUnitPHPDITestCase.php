dee1f5bdb8826263d936e897a33059e07b8528f6
drdamour <drdamour@gmail.com>
2013-10-17 15:38:18 -0600

    Initial implementation

--- src/PhockitoUnit/PhockitoUnitPHPDITestCase.php
+++ src/PhockitoUnit/PhockitoUnitPHPDITestCase.php
@@ -2,17 +2,72 @@
 
 namespace PhockitoUnit;
 
+use DI\ContainerBuilder;
+use DI\Container;
+
 use Phockito;
 
 use PhpDocReader\PhpDocReader;
 use ReflectionClass;
 
+use Doctrine\Common\Cache\Cache;
+
 class PhockitoUnitPHPDITestCase extends PhockitoUnitTestCase
 {
-    public function setUp()
+
+	/**
+	 * @var Container
+	 */
+	protected $DIContainer;
+
+
+
+	public function setUp($useReflection = true, $useAnnotations = true, Cache $cache = null)
     {
         parent::setUp();
+
+	    $builder = new ContainerBuilder();
+	    $builder
+		    ->useReflection($useReflection)
+		    ->useAnnotations($useAnnotations)
+	    ;
+	    if(!is_null($cache)){
+		    $builder->setDefinitionCache($cache);
+	    }
+
+	    $this->DIContainer = $builder->build();
+
+	    $this->registerMocks();
+
     }
+
+	protected function registerMocks(){
+		$parser = new PhpDocReader();
+
+		$class = new ReflectionClass($this);
+
+		$registeredNames = array();
+
+		//Find every member that begins with "mock" or "spy"
+		foreach ($class->getProperties() as $property) {
+
+			if (strpos($property->name, 'mock') === 0 || strpos($property->name, 'spy') === 0) {
+				if($property->name == "mockObjects"){
+					//This is inherited from PHPUnit_Framework_TestCase and we can't mock it
+					continue;
+				}
+
+				//Use the type as the DI name
+				$name = $parser->getPropertyType($property);
+
+				//TODO: Check for duplicates
+				//$this->DIContainer->getDefinitionManager()->getDefinition()
+
+				$property->setAccessible(true);
+				$this->DIContainer->set($name,  $property->getValue( $this ));
+			}
+		}
+	}
 
 
 }
--- tests/UnitTests/PhockitoUnit/PhockitoUnitPHPDITestCaseTest.php
+++ tests/UnitTests/PhockitoUnit/PhockitoUnitPHPDITestCaseTest.php
@@ -3,54 +3,114 @@
 namespace UnitTests\PhockitoUnit;
 
 use Phockito;
-
 use PhockitoUnit\PhockitoUnitPHPDITestCase;
 
-class MockableFacet
+class MockableFacet1
 {
-    public function mockableMethod(){
-        return 5;
-    }
+	public function mockableMethod(){
+		return 5;
+	}
 
-    public function mockableMethod2(){
-        return 5;
-    }
 }
 
+class MockableFacet2
+{
+	public function mockableMethod(){
+		return 5;
+	}
 
+}
+
+class MockableFacet3
+{
+	public function mockableMethod(){
+		return 5;
+	}
+
+}
+
+class SpyableFacet1
+{
+	public function spyableMethod1(){
+		return 5;
+	}
+
+	public function spyableMethod2(){
+		return 5;
+	}
+}
+
+class SpyableFacet2
+{
+	public function spyableMethod1(){
+		return 5;
+	}
+
+	public function spyableMethod2(){
+		return 5;
+	}
+}
+
+class SpyableFacet3
+{
+	public function spyableMethod1(){
+		return 5;
+	}
+
+	public function spyableMethod2(){
+		return 5;
+	}
+}
+
+//We derive from the test case to test it so we can easily access members with protection
+//it seems like this really messes with code coverage
 class PhockitoUnitPHPDITestCaseTest extends PhockitoUnitPHPDITestCase
 {
-    /** @var  MockableFacet */
-    private $mockPrivate;
+	/** @var  MockableFacet1 */
+	private $mockPrivate;
 
-    /** @var  MockableFacet */
-    protected $mockProtected;
+	/** @var  MockableFacet2 */
+	protected $mockProtected;
 
-    /** @var  MockableFacet */
-    public $mockPublic;
+	/** @var  MockableFacet3 */
+	public $mockPublic;
+
+	/** @var  SpyableFacet1 */
+	private $spyPrivate;
+
+	/** @var  SpyableFacet2 */
+	protected $spyProtected;
+
+	/** @var  SpyableFacet3 */
+	public $spyPublic;
+
+	/** @var  MockableFacet1 */
+	public $someOtherThing;
 
 
-    /** @var  MockableFacet */
-    private $spyPrivate;
+	public function setUp()
+	{
+		//do nothing, we'll call the parent's setup explicitly as needed
+	}
 
-    /** @var  MockableFacet */
-    protected $spyProtected;
-
-    /** @var  MockableFacet */
-    public $spyPublic;
-
-    /** @var  MockableFacet */
-    public $someOtherThing;
-
-    public function testMockGeneration()
+    public function testMockGenerationAndRegistry()
     {
 
-        $this->assertNull($this->someOtherThing, "non matching prefix remains");
+	    //everything should start as null
+	    $this->assertNull($this->mockPrivate);
+	    $this->assertNull($this->mockProtected);
+	    $this->assertNull($this->mockPublic);
+        $this->assertNull($this->someOtherThing);
 
+	    parent::setUp();
+
+	    //Now everything prefixed with mock should be set to an instance
         $this->assertNotNull($this->mockPrivate);
         $this->assertNotNull($this->mockProtected);
         $this->assertNotNull($this->mockPublic);
+	    $this->assertNull($this->someOtherThing, "non matching prefix remains");
 
+	    //now try to do some mocking with it
         Phockito::when($this->mockPrivate->mockableMethod())->return("Private");
         Phockito::when($this->mockProtected->mockableMethod())->return("Protected");
         Phockito::when($this->mockPublic->mockableMethod())->return("Public");
@@ -59,32 +119,55 @@ class PhockitoUnitPHPDITestCaseTest extends PhockitoUnitPHPDITestCase
         $this->assertEquals("Protected", $this->mockProtected->mockableMethod() );
         $this->assertEquals("Public", $this->mockPublic->mockableMethod() );
 
+	    //Finally ensure they were appropriately registered in the container
+	    $this->assertSame($this->mockPrivate, $this->DIContainer->get('UnitTests\PhockitoUnit\MockableFacet1'));
+	    $this->assertSame($this->mockProtected, $this->DIContainer->get('UnitTests\PhockitoUnit\MockableFacet2'));
+	    $this->assertSame($this->mockPublic, $this->DIContainer->get('UnitTests\PhockitoUnit\MockableFacet3'));
+
     }
 
 
-    public function testSpyGeneration()
+    public function testSpyGenerationAndRegistry()
     {
+	    //everything should start as null
+	    $this->assertNull($this->spyPrivate);
+	    $this->assertNull($this->spyProtected);
+	    $this->assertNull($this->spyPublic);
+	    $this->assertNull($this->someOtherThing);
 
-        $this->assertNotNull($this->spyPrivate);
-        $this->assertNotNull($this->spyProtected);
-        $this->assertNotNull($this->spyPublic);
+	    parent::setUp();
 
-        Phockito::when($this->spyPrivate->mockableMethod())->return("Private");
-        Phockito::when($this->spyProtected->mockableMethod())->return("Protected");
-        Phockito::when($this->spyPublic->mockableMethod())->return("Public");
+	    //Now everything prefixed with spy should be set to an instance
+	    $this->assertNotNull($this->spyPrivate);
+	    $this->assertNotNull($this->spyProtected);
+	    $this->assertNotNull($this->spyPublic);
 
-        $this->assertEquals("Private", $this->spyPrivate->mockableMethod() );
-        $this->assertEquals("Protected", $this->spyProtected->mockableMethod() );
-        $this->assertEquals("Public", $this->spyPublic->mockableMethod() );
+	    $this->assertNull($this->someOtherThing, "things not prefixed should remain null");
 
-        $this->assertEquals(5, $this->spyPrivate->mockableMethod2() );
-        $this->assertEquals(5, $this->spyProtected->mockableMethod2() );
-        $this->assertEquals(5, $this->spyPublic->mockableMethod2() );
+	    //now try to do some mocking with it
+        Phockito::when($this->spyPrivate->spyableMethod1())->return("Private");
+        Phockito::when($this->spyProtected->spyableMethod1())->return("Protected");
+        Phockito::when($this->spyPublic->spyableMethod1())->return("Public");
 
+        $this->assertEquals("Private", $this->spyPrivate->spyableMethod1() );
+        $this->assertEquals("Protected", $this->spyProtected->spyableMethod1() );
+        $this->assertEquals("Public", $this->spyPublic->spyableMethod1() );
 
+	    //now do some spying with it
+        $this->assertEquals(5, $this->spyPrivate->spyableMethod2() );
+        $this->assertEquals(5, $this->spyProtected->spyableMethod2() );
+        $this->assertEquals(5, $this->spyPublic->spyableMethod2() );
+
+	    //Finally ensure they were appropriately registered in the container
+	    $this->assertSame($this->spyPrivate, $this->DIContainer->get('UnitTests\PhockitoUnit\SpyableFacet1'));
+	    $this->assertSame($this->spyProtected, $this->DIContainer->get('UnitTests\PhockitoUnit\SpyableFacet2'));
+	    $this->assertSame($this->spyPublic, $this->DIContainer->get('UnitTests\PhockitoUnit\SpyableFacet3'));
     }
 
     public function testHamcrestEnabled(){
         $this->assertTrue(anything() instanceof \Hamcrest_Matcher);
     }
+
+
+
 }
